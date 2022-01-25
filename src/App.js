93b6013f4ec6f74aa815f6e3365c704b2e883c78
Sayedfahim <Waud15-7127@diu.edu.bd>
2022-01-25 21:38:41 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Users></Users>
+      <Component></Component>
+      <Component></Component>
+      <Component></Component>
     </div>
   );
 }
+function Users(){
+  const [user, setUser] = useState([]);
+  useEffect( ()=> {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(data => setUser(data))
+  }, [])
+  return(
+    <div className='main'> 
+      <h1>Yo yo mama: {user.length}</h1>
+      {
+        user.map(user => <ComponentUser name={user.name} phone={user.phone}></ComponentUser>)
+      }
+      </div>
+  )
+  }
+function ComponentUser(props){
+  return(
+    <div>
+        <h1>Name: {props.name}</h1>
+        <h3>Phone: {props.phone}</h3>
+    </div>
+  )
+  }
+
+
+function Component(){
+  const [click,setClick] = useState(0);
+
+  const handleAddClick = () => {
+    const newClick = click + 1;
+    setClick(newClick);
+  }
+  const handleAddClick1 = () => {
+    const newClick = click - 1;
+    setClick(newClick);
+  }
+  
+  
+  return(
+    <div className='main'> 
+      <h1>Yo yo mama</h1>
+      <h3>Clicked Me: {click} </h3>
+      <p>Let's test something</p>
+      <button onClick={handleAddClick}>Click Up</button>
+      <button onClick={handleAddClick1}>Click Down</button>
+    </div>
+  )
+}
 
 export default App;
+
